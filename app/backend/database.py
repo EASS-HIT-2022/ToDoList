86f26d1dd707d5116ff4a17e2127476eab21c48c
Neophyte96 <itay55ster@gmail.com>
2022-06-26 14:49:42 +0300

    Added redis database

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,5 +1,5 @@
 import json
-
+import redis
 
 def local_db():    ### Local db
     filename = './data.json'
@@ -10,3 +10,19 @@ def local_db():    ### Local db
         except:  
             db_todo = [] 
             return json.dump(db_todo,file)
+
+
+def convertor(data):
+    if isinstance(data, bytes):  return data.decode('ascii')
+    if isinstance(data, dict):   return dict(map(convertor, data.items()))
+    if isinstance(data, tuple):  return map(convertor, data)
+    return data
+
+redis_con = redis.StrictRedis(host='docker.for.mac.localhost', port=6379)
+
+try:
+    redis_con.ping()
+    print('Connected to redis')
+except:
+    print('Connect to redis failed')
+
